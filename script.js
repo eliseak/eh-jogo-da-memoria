4efc6e19f7c00077520590615c1d527c0630c9d2
Elise <eliseakemi@gmail.com>
2018-06-26 15:54:36 -0300

    Adicionar javascript do jogo da memoria

--- script.js
+++ script.js
@@ -1,1 +1,98 @@
-const cartas = document.querySelectorAll("carta");
+const cartas = document.querySelectorAll(".carta");
+const imagens = ["img/action.jpg", "img/gale.jpg", "img/gale.jpg", "img/action.jpg"];
+const distribuicaoImagens = [];
+let cartasEmbaralhadas = [];
+
+function sortearNumero(min, max){
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+}
+
+function embaralharCartas(){
+
+    for (let carta in cartas){
+        distribuicaoImagens[carta] = 0;
+    }
+
+    for (let carta in cartas){
+        while (cartasEmbaralhadas[carta] == undefined){
+            let num = sortearNumero(0, cartas.length);
+        
+            if (distribuicaoImagens[num] < 2){
+                cartasEmbaralhadas[carta] = imagens[num];
+                distribuicaoImagens[num]++;
+            }
+        }
+    }
+
+}
+
+// Mostra a imagem da carta
+function virarCarta (){
+    if (!this.estaPareada){
+
+        // Mostra a imagem da carta
+        this.style.backgroundImage = `url(${imagens[this.indice]})`;
+        this.estaVirada = true;
+        
+        let cartasViradas = [];
+
+        // Conta o numero de cartas viradas
+        for (let carta of cartas){
+            if (carta.estaVirada && !carta.estaPareada) cartasViradas.push(carta);
+        }
+
+        if (cartasViradas.length === 2){
+            // Verificar par
+            console.log("Verificar par!")
+            verificarPar(cartasViradas);
+        }
+
+    }
+}
+
+// Verificar se as cartas possuem imagens iguais
+function verificarPar(cartas){
+    if (cartas[0].style.backgroundImage == cartas[1].style.backgroundImage){
+        console.log("Cartas iguais!");
+
+        for (let carta of cartas){
+            carta.estaPareada = true; 
+        }
+
+        verificarFimDeJogo();
+    }
+    else{
+        console.log("Cartas diferentes!");
+
+        // (Des)vira a carta
+        for (let carta of cartas){
+            carta.style.backgroundImage = "url(img/reverse_clear.png)";
+            carta.estaVirada = false;
+        }
+
+    }
+}
+
+// Verifica se todas as cartas foram viradas
+function verificarFimDeJogo(){
+    let todasPareadas = true;
+
+    for (let carta of cartas){
+        if (!carta.estaPareada) todasPareadas = false;
+    }
+
+    if (todasPareadas){
+        console.log("Fim do jogo!");
+    }
+}
+
+embaralharCartas();
+
+// Atribuições
+for (let carta in cartas){
+    cartas[carta].onclick = virarCarta;
+    cartas[carta].estaVirada = false;
+    cartas[carta].estaPareada = false; 
+    cartas[carta].indice = carta; 
+}
+
